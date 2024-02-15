399fed1ad2cd84eb619ecfbb7d3560e05749d89f
Prasanta Ku Sethi <sethip52@gmail.com>
2024-02-16 00:50:46 +0530

    initial commit - db and server setup done

---
+++ server/config/db-config.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const colors = require('colors')
+
+const connectDB = async() => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log(`Connected to database ${mongoose.connection.host}`.bgGreen.white);
+    } catch (error) {
+        console.log(`Error in connecting db: ${error}`.bgRed.white);
+    }
+}
+
+module.exports = connectDB;
---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const cors = require('cors')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const colors = require('colors')
+const connectDB = require('./config/db-config')
+
+// dotenv configure
+dotenv.config()
+
+// mongoDB connection
+connectDB();
+
+// rest object creation
+const app = express()
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+// server listen
+app.listen(process.env.PORT || 3001, '192.168.158.6', () => {
+    console.log(`server running on port ${process.env.PORT}...`.bgGreen.white);
+})
