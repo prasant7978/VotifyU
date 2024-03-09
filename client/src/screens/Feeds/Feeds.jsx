e06e3cd0dec197cccc765a62694081d974878b0a
Prasanta Ku Sethi <sethip52@gmail.com>
2024-03-09 21:06:23 +0530

    pagination added for all posts in feeds page

--- client/src/screens/Feeds/Feeds.jsx
+++ client/src/screens/Feeds/Feeds.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 
 import styles from './style';
 
@@ -15,18 +15,65 @@ const Home = () => {
   // global states
   const [allPosts, setAllPosts, fetchAllPosts] = useContext(PostContext);
 
+  const userPostPageSize = 3;
+  const [userPostscurrentPage, setUserPostsCurrentPage] = useState(1);
+  const [userPostsRenderData, setUserPostsRenderData] = useState([]);
+  const [isLoadingUserPosts, setIsLoadingUserPosts] = useState(false);
+
+  const pagination = (allPosts, currentPage, pageSize) => {
+    console.log('current page: ', currentPage);
+    let startIndex = (currentPage - 1) * pageSize;
+    let endIndex = startIndex + pageSize;
+
+    if(startIndex >= endIndex)
+      return [];
+
+    return allPosts.slice(startIndex, endIndex);
+  }
+
   useEffect(() => {
     const fetchData = async () => {
       await fetchAllPosts('feed page');
     };
 
-    fetchData();
+    fetchData()
+
+    // setIsLoadingUserPosts(true);
+    // const initialPosts = pagination(allPosts, 1, userPostPageSize);
+    // console.log('all posts: ', allPosts);
+    // console.log('initial posts: ', initialPosts);
+    // setUserPostsRenderData(initialPosts);
+    // setIsLoadingUserPosts(false);
   }, [setAllPosts]);
+
+  // initial loading of posts
+  useEffect(() => {
+    setIsLoadingUserPosts(true);
+    const initialPosts = pagination(allPosts, 1, userPostPageSize);
+    setUserPostsRenderData(initialPosts);
+    setIsLoadingUserPosts(false);
+  }, [allPosts]);
 
   return (
     <View style={[globalStyles.whiteBackground, globalStyles.flex, styles.container]}>
       <FlatList
-        data={allPosts}
+        data={userPostsRenderData}
+        onEndReachedThreshold={0.5}
+        onEndReached={() => {
+          if(isLoadingUserPosts == true)
+            return;
+
+          setIsLoadingUserPosts(true);
+
+          console.log('fetching posts for page: ', userPostscurrentPage + 1);
+          const newPosts = pagination(allPosts, userPostscurrentPage + 1, userPostPageSize);
+          if(newPosts.length > 0){
+            setUserPostsCurrentPage(userPostscurrentPage + 1);
+            setUserPostsRenderData(prev => [...prev, ...newPosts]);
+          }
+
+          setIsLoadingUserPosts(false);
+        }}
         showsVerticalScrollIndicator={false}
         renderItem={({item}) => <PostCard post={item}/>}
       />
